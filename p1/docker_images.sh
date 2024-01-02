e60fc3c699f06a3b49e15673783f9dcc0a805291
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-02 10:48:09 +0100

    Update docker_images.sh

--- p1/docker_images.sh
+++ p1/docker_images.sh
@@ -1,2 +1,2 @@
-sudo docker build -t frrouting -f ./frrouting_docker_file .
-sudo docker build -t alpine -f ./alpine_docker_file .
+docker build -t frrouting -f ./frrouting_docker_file .
+docker build -t alpine -f ./alpine_docker_file .
