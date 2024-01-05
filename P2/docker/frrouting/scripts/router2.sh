79500ec04ff521d9e2c73174660a4c796d82848d
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-05 13:17:24 +0100

    Add VXLAN configuration for Unicast and Multicast

--- P2/docker/frrouting/scripts/router2.sh
+++ P2/docker/frrouting/scripts/router2.sh
@@ -2,16 +2,31 @@
 
 echo "Warning : Use Bash not sh !!!"
 
+# Prompt for unicast or multicast
+read -p "Activate Unicast (u) or Multicast (m)? " choice
+
 # vxlan config
 ip link add br0 type bridge
 ip link set dev br0 up
 ip addr add 10.1.1.2/24 dev eth0
 
-# VXLAN unicast 
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
+case $choice in
+  u|U)
+    echo "Setting up Unicast..."
 
-# VXLAN multicast
-#ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+	# VXLAN unicast 
+	ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
+	;;
+  m|M)
+	echo "Setting up Multicast..."
+	# VXLAN multicast
+	ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+ 	;;
+	*)
+    echo "Invalid choice. Please run the script again and choose either 'u' for Unicast or 'm' for Multicast."
+    exit 1
+    ;;
+esac
 
 ip addr add 20.1.1.2/24 dev vxlan10
 
