e057136c1660d3c08273da9e6bdd94a82ff02cd8
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-01-23 10:51:57 +0100

    added comments and formats

--- P3/docker/frrouting/scripts/router4.sh
+++ P3/docker/frrouting/scripts/router4.sh
@@ -2,38 +2,39 @@
 
 echo "Warning : Use Bash not sh !!!"
 
-brctl addbr br0
-ip link set up dev br0
-ip link add vxlan10 type vxlan id 10 dstport 4789
-ip link set up dev vxlan10
-brctl addif br0 vxlan10
+ip link add name vxlan10 type vxlan id 10 dev eth2 dstport 4789
+ip link set dev vxlan10 up
+ip link add name br0 type bridge
+ip link set dev br0 up
 brctl addif br0 eth0
-
+brctl addif br0 vxlan10
 
 vtysh
 conf t
 
-no ipv6 forwarding
+	no ipv6 forwarding
 
-interface eth2
-ip address 10.1.1.10/30
-ip ospf area 0
+	interface eth2
+		ip address 10.1.1.10/30
+		ip ospf area 0
+	exit
 
-interface lo
-ip address 1.1.1.4/32
+	interface lo
+		ip address 1.1.1.4/32
+		ip ospf area 0
+	exit
 
-ip ospf area 0
+	router bgp 1
+		neighbor 1.1.1.1 remote-as 1
+		neighbor 1.1.1.1 update-source lo
 
-router bgp 1
-neighbor 1.1.1.1 remote-as 1
-neighbor 1.1.1.1 update-source lo
-
-address-family l2vpn evpn
-neighbor 1.1.1.1 activate
-advertise-all-vni
-exit-address-family
-
-router ospf
+		 address-family l2vpn evpn
+			neighbor 1.1.1.1 activate
+			advertise-all-vni
+		exit-address-family
+	exit
+	router ospf
+exit
 
 end
 write memory
